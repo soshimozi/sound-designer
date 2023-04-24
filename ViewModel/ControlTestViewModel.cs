65e23672166c3d0dd365ddd2b962462dd441b4dc
Soshimozi <scott.mccain@gmail.com>
2023-04-23 23:26:01 -0700

    Working on Cable integration

---
+++ ViewModel/ControlTestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SoundDesigner.Lib;
+using System.Collections.ObjectModel;
+using SoundDesigner.Models;
+
+namespace SoundDesigner.ViewModel;
+
+public class ControlTestViewModel : ViewModelBase
+{
+    public ObservableCollection<CableConnection> Connections { get; set; } = new ObservableCollection<CableConnection>();
+}
